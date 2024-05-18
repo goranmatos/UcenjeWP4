9ead32dbd4186f8c59b5879f2df0d37cb9be53ed
goranmatos <goran.matos@a1.hr>
2024-05-18 10:48:05 +0200

    Narebe za insert vjezbanje

---
+++ 20240517_Vjezbanje.sql
@@ -0,0 +1,7 @@
+select * from smjerovi;
+
+--Školska sintaksa
+insert into smjerovi (naziv, trajanje, cijena, izvodiseod, verificiran)
+values	('Web programiranje', 225, 1555.99, '2024-01-15', 1);
+
+select * from smjerovi;
