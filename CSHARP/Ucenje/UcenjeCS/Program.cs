35b01548b85fcf27e0a50b443eab97a79d604b1d
goranmatos <goran.matos@a1.hr>
2024-06-07 20:34:01 +0200

    ucenje

---
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+*
+*komentar kroz
+*vise redova
+*
+*/
+Console.WriteLine("Hello, World!!!!!");
+Console.WriteLine(7);
+Console.WriteLine("Goran Matoš");
