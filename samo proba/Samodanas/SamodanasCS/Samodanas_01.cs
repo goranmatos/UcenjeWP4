12b202ce078f9ae0d9d89a0b34b5ba4aa1c4c229
goranmatos <goran.matos@a1.hr>
2024-06-16 20:50:14 +0200

    vjezba

---
+++ samo proba/Samodanas/SamodanasCS/Samodanas_01.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SamodanasCS
+{
+    internal class Samodanas_01
+    {
+
+        public static void Izvedi()
+        {
+            Console.WriteLine("evo me smao dans 01");
+        }
+    }
+}
